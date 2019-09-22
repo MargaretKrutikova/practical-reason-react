3ce0cbdccf90d219d263cb9cbded3dd5754aa5de
Margarita Krutikova <margaretkru@gmail.com>
2019-09-22 17:08:16 +0200

    Edit build path

--- context/webpack.config.js
+++ context/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const outputDir = path.join(__dirname, "build/")
+const outputDir = path.join(__dirname, "../build/context")
 
 const isProd = process.env.NODE_ENV === "production"
 
